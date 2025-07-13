946b99692dc4c7df996f30dc736c80a9dcf9ee6f
nicolerodriguezsmc <rodriguez_nicole_l01@student.smc.edu>
2025-07-13 15:57:04 -0700

    Update hobbyTracker.js with more descriptive comments

--- hobbyTracker.js
+++ hobbyTracker.js
@@ -9,25 +9,31 @@ const hobbyLog = [
 
 // Calculate the total time spent on all hobbies
 function totalTime(log) {
+  // Add up all the minutes from each hobby session using reduce
   return log.reduce((sum, session) => sum + session.minutes, 0);
 }
 
 // Create a list of unique hobbies that have no duplicates
 function uniqueHobbies(log) {
+   // Extract hobby names using map
   const names = log.map(entry => entry.hobby);
+  //Remove duplicates using Set
   return [...new Set(names)];
 }
 
 // Find sessions that lasted longer than a certain number of minutes
 function longSessions(log, minMinutes) {
+  // Use filter to return the sessions that have more minutes than minMinutes
   return log.filter(entry => entry.minutes > minMinutes);
 }
 
 // Count how many times a specific mood appears in the log
 function countMood(log, moodType) {
+  // Return the number of sessions that match the given mood using filter
   return log.filter(entry => entry.mood === moodType).length;
 }
 
+//Print results to console
 console.log("Total time spent:", totalTime(hobbyLog), "minutes");
 console.log("Unique hobbies:", uniqueHobbies(hobbyLog));
 console.log("Sessions longer than 30 min:", longSessions(hobbyLog, 30));
